c3c2976e2c258c29ab1343b847294688a6c5a92c
le thanh chung <ltchung.dhkl17a3hn@sv.uneti.edu.vn>
2023-10-30 02:07:26 -0700

    add file hello.py

---
+++ hello.py
@@ -0,0 +1,28 @@
+## bai 3
+ten_hang= "sua hop vinamilk"
+so_luong= 5
+don_gia=25000
+sotientra= so_luong*don_gia
+print("so tien phai tra la")
+print(so_luong,"*",don_gia,"=",sotientra)
+## bai 2
+cac_phep_toan_don_gian="X,Y"
+X=10
+Y=5
+tong=X+Y
+hieu=X-Y
+tich=X*Y
+thuong=X/Y
+print("tong hieu tich thuong cua cac phep tinh la")
+print(X ,"+", Y ,"=",tong )
+print(X ,"-", Y ,"=",hieu) 
+print(X ,"*", Y ,"=",tich )
+print(X ,"/", Y ,"=",thuong )
+## bai 1
+Hello="""
+** ** ****** ** ** ** ****
+** ** ** ** ** ** **
+****** ***** ** ** ** **
+** ** ** ** ** ** **
+** ** ** **** ****** ****** ******"""
+print("Hello")
